20279c78c831f6b1fdfc990a8c479eb2452958da
pranavid <damarlapranavi@gmail.com>
2021-11-24 22:21:13 +0530

    Added all the database entities

--- csia_app/admin.py
+++ csia_app/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
 from .models import TaskStatus
+from .models import Subject
+from .models import Student
+from .models import SubjectStudent
+from .models import Task
 
 admin.site.register(TaskStatus)
+admin.site.register(Subject)
+admin.site.register(Student)
+admin.site.register(SubjectStudent)
+admin.site.register(Task)
--- csia_app/models.py
+++ csia_app/models.py
@@ -1,7 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class TaskStatus(models.Model):
     status = models.CharField(max_length=10)
 
     def __str__(self):
         return f"{self.status}"
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=15)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Student(models.Model):
+     user = models.OneToOneField(User, on_delete=models.CASCADE)
+     name = models.CharField(max_length=20)
+     username = models.CharField(max_length=20)
+     email = models.EmailField
+
+     def __str__(self):
+         return f"{self.name}, {self.username}"
+
+class SubjectStudent(models.Model):
+     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+     student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+     def __str__(self):
+         return f"{self.subject}, {self.student}"
+
+class Task(models.Model):
+     name = models.CharField(max_length=100)
+     description = models.TextField()
+     deadline = models.DateTimeField()
+     status = models.ForeignKey(TaskStatus, on_delete=models.CASCADE)
+     student = models.ForeignKey(Student, on_delete=models.CASCADE)
+     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+     file = models.FileField(upload_to= "static/", null=True, blank=True)
+     grade = models.IntegerField(null=True)
+
+     def __str__(self):
+         return f"{self.name}, {self.deadline}, {self.subject}"
